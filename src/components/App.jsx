ab372402ebcfb18a94c9e59a78518ec4d51c6b0f
Viacheslav Kobets <mertag_nu@ukr.net>
2023-04-09 13:40:56 +0300

    done homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { nanoid } from 'nanoid';
+import { Div, Frame, Title } from './App.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter, getFilter } from '../redux/Slices/filterSlice';
+import { add, remove, getContacts } from '../redux/Slices/contactsSlice';
+
+export function App() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filterValue = useSelector(getFilter);
+
+  const onContactFormSubmit = (newContact, { resetForm }) => {
+    const alreadyExist = contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (alreadyExist) {
+      resetForm();
+      return alert(`${newContact.name} is already in contacts`);
+    }
+
+    const contact = { id: nanoid(), ...newContact };
+
+    dispatch(add(contact));
+    resetForm();
+  };
+
+  const FilterContacts = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  const onDeleteContact = id => {
+    dispatch(remove(id));
+  };
+
+  const normalizedFiler = filterValue.toLowerCase();
+  const filteredContacts = contacts.filter(({ name }) =>
+    name.toLowerCase().includes(normalizedFiler)
   );
-};
+
+  return (
+    <Frame>
+      <Div>
+        <Title>Phonebook</Title>
+        <ContactForm onContactFormSubmit={onContactFormSubmit} />
+
+        <h2>Contacts</h2>
+        <Filter filter={filterValue} OnChange={FilterContacts} />
+
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={onDeleteContact}
+        />
+      </Div>
+    </Frame>
+  );
+}
---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const FilterLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 18px;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+export const FilterInput = styled.input`
+  width: 200px;
+  height: 30px;
+  margin-top: 10px;
+  border-radius: 10px;
+  font-size: 16px;
+`;
